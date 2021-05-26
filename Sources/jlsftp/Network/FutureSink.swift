683d10c4b7fe1a47629d73d3cc3718e4c007dba3
Unknown <jman012guy@gmail.com>
2021-05-25 17:56:22 -0700

    Add comments to FutureSink

--- Sources/jlsftp/Network/FutureSink.swift
+++ Sources/jlsftp/Network/FutureSink.swift
@@ -4,12 +4,20 @@ import NIO
 
 extension Publisher {
 
+	/**
+	 A custom Combine sink that creates SwiftNIO Futures when processing inputs.
+	 As Futures are created, they are stored in the sink until they are completed.
+	 As Futures are completed, they are cleared from the store. If the store of
+	 Futures is full, the sink uses backpressure to tell the upstream that no
+	 more input values can be processed. This is handled by a non-infinite demand.
+	 */
 	func futureSink(
 		maxConcurrent: UInt,
 		receiveCompletion: @escaping (Subscribers.Completion<Failure>) -> Void,
 		receiveValue: @escaping FutureSink<Self.Output, Self.Failure>.Handler
 	) -> FutureSink<Self.Output, Self.Failure> {
 		let futureSink = FutureSink(maxConcurrent: maxConcurrent, receiveCompletion: receiveCompletion, receiveValue: receiveValue)
+		// Immediately subscribe the futureSink to this publisher.
 		subscribe(futureSink)
 		return futureSink
 	}
@@ -19,13 +27,22 @@ public class FutureSink<Input, Failure: Error> {
 
 	public typealias Handler = (Input) -> EventLoopFuture<Void>
 
+	/// The maximum number of oustanding Futures this sink can hold.
 	private let maxConcurrent: UInt
+	/// The sink input value handler that returns a Future.
 	private let receiveValue: Handler
+	/// The sink completion handler.
 	private let receiveCompletion: (Subscribers.Completion<Failure>) -> Void
 
+	/// Internal tracker for how many Futures are current outstanding.
 	private var currentConcurrent: UInt = 0
+	/// The subscription to the upstream publisher.
 	private var subscription: Subscription?
 
+	/**
+	 The current amount of demand for the upstream publisher, based on
+	 max and current Futures.
+	 */
 	private var currentDemand: Subscribers.Demand {
 		.max(Int(maxConcurrent - currentConcurrent))
 	}
@@ -47,16 +64,23 @@ public class FutureSink<Input, Failure: Error> {
 
 extension FutureSink: Subscriber {
 	public func receive(subscription: Subscription) {
+		// We've been subscribed to an upstream. Store the subscription, then
+		// request the initial demand.
 		self.subscription = subscription
 		self.subscription?.request(currentDemand)
 	}
 
 	public func receive(_ input: Input) -> Subscribers.Demand {
+		// If so, then send the input to the handler and track
+		// the returned Future.
 		currentConcurrent += 1
 		receiveValue(input).whenComplete { _ in
+			// When a Future is resolved, stop tracking it
 			self.currentConcurrent -= 1
+			// And ask for the additional demand from upstream
 			self.subscription?.request(.max(1))
 		}
+		// No additional demand.
 		return .none
 	}
 
--- Tests/jlsftpTests/Network/FutureSinkTests.swift
+++ Tests/jlsftpTests/Network/FutureSinkTests.swift
@@ -6,19 +6,26 @@ import NIO
 final class FutureSinkTests: XCTestCase {
 
 	func testValid() {
+		// Need a channel for Futures
 		let channel = EmbeddedChannel()
+		// Store received promises/futures in here
 		var promises = CircularBuffer<EventLoopPromise<Void>>()
+		// Store received values in here
 		var receivedValues: [Int] = []
 
 		let passthroughSubj = PassthroughSubject<Int, Never>()
 		let futureSink = FutureSink<Int, Never>(maxConcurrent: 3, receiveCompletion: { _ in }, receiveValue: {
+			// Record the value
 			receivedValues.append($0)
+			// Make a new promise and track it
 			let p = channel.eventLoop.makePromise(of: Void.self)
 			promises.append(p)
 			return p.futureResult
 		})
 		passthroughSubj.subscribe(futureSink)
 
+		// First, some standard tracking of received values and promises after
+		// sending values.
 		XCTAssertEqual(receivedValues, [])
 		XCTAssertEqual(promises.count, 0)
 		passthroughSubj.send(1)
@@ -30,9 +37,13 @@ final class FutureSinkTests: XCTestCase {
 		passthroughSubj.send(3)
 		XCTAssertEqual(receivedValues, [1, 2, 3])
 		XCTAssertEqual(promises.count, 3)
+		// The sink is full and has no more demand from upstream.
+		// Attempting to send a value should fail silently with no changed data.
 		passthroughSubj.send(4)
 		XCTAssertEqual(receivedValues, [1, 2, 3])
 		XCTAssertEqual(promises.count, 3)
+		// Next, mark a Future as completed, which should free up some space
+		// in the sink. Sending 5 should be tracked now.
 		promises.popFirst()!.succeed(())
 		XCTAssertEqual(receivedValues, [1, 2, 3])
 		XCTAssertEqual(promises.count, 2)
