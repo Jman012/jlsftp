2810b776645f898183fb083bee06477f58cb526e
Unknown <jman012guy@gmail.com>
2021-07-07 17:36:05 -0700

    Organize BaseSftpHandler tests and handlers

--- Tests/jlsftpTests/Server/BaseSftpServerTests.swift
+++ Tests/jlsftpTests/Server/BaseSftpServerTests.swift
@@ -53,71 +53,9 @@ final class BaseSftpServerTests: XCTestCase {
 	}
 
 	func testHandleOpenClose() {
-		let group = MultiThreadedEventLoopGroup(numberOfThreads: 1)
-		defer {
-			try! group.syncShutdownGracefully()
+		_testWithTemporaryFile(content:"abc", openFlags: [.read]) { _, _, _, _ in
+			// Do nothing. Just open and close the file.
 		}
-		let eventLoop = group.next()
-		let threadPool = NIOThreadPool(numberOfThreads: 1)
-		threadPool.start()
-		defer {
-			try! threadPool.syncShutdownGracefully()
-		}
-		let server = BaseSftpServer(forVersion: .v3, threadPool: threadPool, logger: Logger(label: "test"))
-
-		XCTAssertNoThrow(try withTemporaryFile(content: "abc") { _, filePath in
-			// Open temporary file
-			let openPacket: OpenPacket = .init(
-				id: 1,
-				filename: filePath,
-				pflags: [.read],
-				fileAttributes: .empty)
-			var lastReplyMessage: SftpMessage?
-			let replyHandler: ReplyHandler = { message in
-				lastReplyMessage = message
-				return eventLoop.makeSucceededFuture(())
-			}
-			server.register(replyHandler: replyHandler)
-			var message = SftpMessage(packet: .open(openPacket), dataLength: 0, shouldReadHandler: { _ in })
-			XCTAssertNoThrow(try server.handle(message: message, on: eventLoop).wait())
-
-			// Assert correct reply, extract handle
-			guard let openReply = lastReplyMessage else {
-				XCTFail()
-				assert(false)
-			}
-			var sftpHandle: String?
-			switch openReply.packet {
-			case let .handleReply(handleReply):
-				XCTAssertEqual(handleReply.id, 1)
-				sftpHandle = handleReply.handle
-			default:
-				XCTFail()
-			}
-			guard let sftpHandleString = sftpHandle else {
-				XCTFail()
-				assert(false)
-			}
-
-			// Use handle to close temporary file
-			lastReplyMessage = nil
-			let closePacket: ClosePacket = .init(id: 2, handle: sftpHandleString)
-			message = SftpMessage(packet: .close(closePacket), dataLength: 0, shouldReadHandler: { _ in })
-			XCTAssertNoThrow(try server.handle(message: message, on: eventLoop).wait())
-
-			// Assert correct reply
-			guard let closeReply = lastReplyMessage else {
-				XCTFail()
-				assert(false)
-			}
-			switch closeReply.packet {
-			case let .statusReply(statusReply):
-				XCTAssertEqual(statusReply.id, 2)
-				XCTAssertEqual(statusReply.statusCode, .ok)
-			default:
-				XCTFail()
-			}
-		})
 	}
 
 	func testHandleReadSimple() {
@@ -138,62 +76,15 @@ final class BaseSftpServerTests: XCTestCase {
 	}
 
 	func _testHandleRead(content: String, offset: UInt64, length: UInt32, expect: String) {
-		let group = MultiThreadedEventLoopGroup(numberOfThreads: 1)
-		defer {
-			try! group.syncShutdownGracefully()
-		}
-		let eventLoop = group.next()
-		let threadPool = NIOThreadPool(numberOfThreads: 1)
-		threadPool.start()
-		defer {
-			try! threadPool.syncShutdownGracefully()
-		}
-		let server = BaseSftpServer(forVersion: .v3, threadPool: threadPool, logger: Logger(label: "test"))
-
-		XCTAssertNoThrow(try withTemporaryFile(content: content) { _, filePath in
-			// Open temporary file
-			let openPacket: OpenPacket = .init(
-				id: 1,
-				filename: filePath,
-				pflags: [.read],
-				fileAttributes: .empty)
-			var lastReplyMessage: SftpMessage?
-			var lastReplyPromise: EventLoopPromise<Void> = eventLoop.makePromise()
-			var replyHandler: ReplyHandler = { message in
-				lastReplyMessage = message
-				return lastReplyPromise.futureResult
-			}
-			lastReplyPromise.completeWith(.success(()))
-			server.register(replyHandler: replyHandler)
-			var message = SftpMessage(packet: .open(openPacket), dataLength: 0, shouldReadHandler: { _ in })
-			XCTAssertNoThrow(try server.handle(message: message, on: eventLoop).wait())
-
-			// Assert correct reply, extract handle
-			guard let openReply = lastReplyMessage else {
-				XCTFail()
-				assert(false)
-			}
-			var sftpHandle: String?
-			switch openReply.packet {
-			case let .handleReply(handleReply):
-				XCTAssertEqual(handleReply.id, 1)
-				sftpHandle = handleReply.handle
-			default:
-				XCTFail()
-			}
-			guard let sftpHandleString = sftpHandle else {
-				XCTFail()
-				assert(false)
-			}
-
+		_testWithTemporaryFile(content: content, openFlags: [.read]) { sftpHandleString, _, eventLoop, server in
 			// Use handle to read temporary file
-			lastReplyMessage = nil
-			lastReplyPromise = eventLoop.makePromise()
+			var lastReplyMessage: SftpMessage? = nil
+			let lastReplyPromise: EventLoopPromise<Void> = eventLoop.makePromise()
 			var accumulatedBuffer = ByteBuffer()
 			var cancellable: AnyCancellable?
 			var promises: [EventLoopPromise<Void>] = []
 			let promiseLock = DispatchSemaphore(value: 1)
-			replyHandler = { message in
+			let replyHandler: ReplyHandler = { message in
 				lastReplyMessage = message
 				cancellable = message.data.futureSink(maxConcurrent: 10, receiveCompletion: { completion, outstanding in
 					EventLoopFuture
@@ -213,7 +104,7 @@ final class BaseSftpServerTests: XCTestCase {
 
 			let readPacket: ReadPacket = .init(id: 2, handle: sftpHandleString, offset: offset, length: length)
 			server.register(replyHandler: replyHandler)
-			message = SftpMessage(packet: .read(readPacket), dataLength: 0, shouldReadHandler: { _ in })
+			let message = SftpMessage(packet: .read(readPacket), dataLength: 0, shouldReadHandler: { _ in })
 
 			// Every 50ms, succeed the queued promises from reading. This should trigger
 			// backpressure inside of BaseSftpServer.handleRead(:::) for code coverage.
@@ -246,84 +137,41 @@ final class BaseSftpServerTests: XCTestCase {
 			XCTAssertNotNil(cancellable)
 			XCTAssertEqual(accumulatedBuffer.getString(at: 0, length: accumulatedBuffer.readableBytes), expect)
 
-		})
+		}
 	}
 
 	func testHandleWriteSimple() {
-		_testHandleWrite(content: "abc", offset: 0, expect: "abc")
+		_testHandleWrite(initialContent: "", contentToWrite: "abc", offset: 0, expect: "abc")
 	}
 
 	func testHandleWriteOffset() {
-		_testHandleWrite(content: "abc", offset: 1, expect: "\0abc")
+		_testHandleWrite(initialContent: "", contentToWrite: "abc", offset: 1, expect: "\0abc")
+	}
+
+	func testHandleWriteOverwrite() {
+		_testHandleWrite(initialContent: "abc", contentToWrite: "abc", offset: 1, expect: "aabc")
 	}
 
 	func testHandleWriteLargeFile() {
 		let largeFileContent: String = String(repeating: "Hello ", count: 1_000)
-		_testHandleWrite(content: largeFileContent, offset: 0, expect: largeFileContent)
+		_testHandleWrite(initialContent: "", contentToWrite: largeFileContent, offset: 0, expect: largeFileContent)
 	}
 
-	func _testHandleWrite(content: String, offset: UInt64, expect: String) {
-		let group = MultiThreadedEventLoopGroup(numberOfThreads: 1)
-		defer {
-			try! group.syncShutdownGracefully()
-		}
-		let eventLoop = group.next()
-		let threadPool = NIOThreadPool(numberOfThreads: 1)
-		threadPool.start()
-		defer {
-			try! threadPool.syncShutdownGracefully()
-		}
-		let server = BaseSftpServer(forVersion: .v3, threadPool: threadPool, logger: Logger(label: "test"))
-
-		XCTAssertNoThrow(try withTemporaryFile(content: "") { _, filePath in
-			// Open temporary file
-			let openPacket: OpenPacket = .init(
-				id: 1,
-				filename: filePath,
-				pflags: [.write],
-				fileAttributes: .empty)
-			var lastReplyMessage: SftpMessage?
-			var lastReplyPromise: EventLoopPromise<Void> = eventLoop.makePromise()
-			var replyHandler: ReplyHandler = { message in
-				lastReplyMessage = message
-				return lastReplyPromise.futureResult
-			}
-			lastReplyPromise.completeWith(.success(()))
-			server.register(replyHandler: replyHandler)
-			var message = SftpMessage(packet: .open(openPacket), dataLength: 0, shouldReadHandler: { _ in })
-			XCTAssertNoThrow(try server.handle(message: message, on: eventLoop).wait())
-
-			// Assert correct reply, extract handle
-			guard let openReply = lastReplyMessage else {
-				XCTFail()
-				assert(false)
-			}
-			var sftpHandle: String?
-			switch openReply.packet {
-			case let .handleReply(handleReply):
-				XCTAssertEqual(handleReply.id, 1)
-				sftpHandle = handleReply.handle
-			default:
-				XCTFail()
-			}
-			guard let sftpHandleString = sftpHandle else {
-				XCTFail()
-				assert(false)
-			}
-
+	func _testHandleWrite(initialContent: String, contentToWrite: String, offset: UInt64, expect: String) {
+		_testWithTemporaryFile(content: initialContent, openFlags: [.write]) { sftpHandleString, filePath, eventLoop, server in
 			// Use handle to write to temporary file
-			lastReplyMessage = nil
-			lastReplyPromise = eventLoop.makePromise()
+			var lastReplyMessage: SftpMessage? = nil
+			let lastReplyPromise: EventLoopPromise<Void> = eventLoop.makePromise()
 			lastReplyPromise.succeed(())
-			replyHandler = { message in
+			let replyHandler: ReplyHandler = { message in
 				lastReplyMessage = message
 				return lastReplyPromise.futureResult
 			}
 			server.register(replyHandler: replyHandler)
 			let writePacket: WritePacket = .init(id: 2, handle: sftpHandleString, offset: offset)
 			var shouldWrite = false
-			message = SftpMessage(packet: .write(writePacket),
-								  dataLength: UInt32(content.utf8.count),
+			let message = SftpMessage(packet: .write(writePacket),
+								  dataLength: UInt32(contentToWrite.utf8.count),
 								  shouldReadHandler: { should in
 									shouldWrite = should
 								  })
@@ -331,14 +179,14 @@ final class BaseSftpServerTests: XCTestCase {
 			let replyFuture = server.handle(message: message, on: eventLoop)
 
 			// Write data to message
-			var index = content.utf8.startIndex
-			while index < content.utf8.endIndex {
+			var index = contentToWrite.utf8.startIndex
+			while index < contentToWrite.utf8.endIndex {
 				if shouldWrite {
-					let result = message.sendData(ByteBuffer(bytes: [content.utf8[index]]))
-					index = content.utf8.index(index, offsetBy: 1)
+					let result = message.sendData(ByteBuffer(bytes: [contentToWrite.utf8[index]]))
+					index = contentToWrite.utf8.index(index, offsetBy: 1)
 					switch result {
 					case let .success(isComplete):
-						XCTAssertEqual(isComplete, index == content.utf8.endIndex)
+						XCTAssertEqual(isComplete, index == contentToWrite.utf8.endIndex)
 					default:
 						XCTFail()
 					}
@@ -372,7 +220,7 @@ final class BaseSftpServerTests: XCTestCase {
 			expect2.withUTF8 { buffer in
 				XCTAssertEqual(Data(buffer: buffer), fileData!)
 			}
-		})
+		}
 	}
 
 	static var allTests = [
@@ -382,4 +230,99 @@ final class BaseSftpServerTests: XCTestCase {
 		("testHandleReadEof", testHandleReadEof),
 		("testHandleReadLargeFile", testHandleReadLargeFile),
 	]
+}
+
+extension BaseSftpServerTests {
+	// Common functions
+
+	func __openFile(filePath: String, openFlags: OpenFlags, eventLoop: EventLoop, server: BaseSftpServer) -> String {
+		// Open temporary file
+		let openPacket: OpenPacket = .init(
+			id: 1,
+			filename: filePath,
+			pflags: openFlags,
+			fileAttributes: .empty)
+		var lastReplyMessage: SftpMessage?
+		let replyHandler: ReplyHandler = { message in
+			lastReplyMessage = message
+			return eventLoop.makeSucceededVoidFuture()
+		}
+		server.register(replyHandler: replyHandler)
+		let message = SftpMessage(packet: .open(openPacket), dataLength: 0, shouldReadHandler: { _ in })
+		XCTAssertNoThrow(try server.handle(message: message, on: eventLoop).wait())
+
+		// Assert correct reply, extract handle
+		guard let openReply = lastReplyMessage else {
+			XCTFail()
+			assert(false)
+		}
+		var sftpHandle: String?
+		switch openReply.packet {
+		case let .handleReply(handleReply):
+			XCTAssertEqual(handleReply.id, 1)
+			sftpHandle = handleReply.handle
+		default:
+			XCTFail()
+		}
+		guard let sftpHandleString = sftpHandle else {
+			XCTFail()
+			assert(false)
+		}
+
+		return sftpHandleString
+	}
+
+	func __closeFile(sftpHandleString: String, eventLoop: EventLoop, server: BaseSftpServer) {
+		// Use handle to close temporary file
+		var lastReplyMessage: SftpMessage? = nil
+		server.register(replyHandler: { message in
+			lastReplyMessage = message
+			return eventLoop.makeSucceededVoidFuture()
+		})
+		let closePacket: ClosePacket = .init(id: 2, handle: sftpHandleString)
+		let message = SftpMessage(packet: .close(closePacket), dataLength: 0, shouldReadHandler: { _ in })
+		XCTAssertNoThrow(try server.handle(message: message, on: eventLoop).wait())
+
+		// Assert correct reply
+		guard let closeReply = lastReplyMessage else {
+			XCTFail()
+			assert(false)
+		}
+		switch closeReply.packet {
+		case let .statusReply(statusReply):
+			XCTAssertEqual(statusReply.id, 2)
+			XCTAssertEqual(statusReply.statusCode, .ok)
+		default:
+			XCTFail()
+		}
+	}
+
+	func __withServer(_ body: (EventLoop, BaseSftpServer) throws -> Void) {
+		let group = MultiThreadedEventLoopGroup(numberOfThreads: 1)
+		defer {
+			try! group.syncShutdownGracefully()
+		}
+		let eventLoop = group.next()
+		let threadPool = NIOThreadPool(numberOfThreads: 1)
+		threadPool.start()
+		defer {
+			try! threadPool.syncShutdownGracefully()
+		}
+		let server = BaseSftpServer(forVersion: .v3, threadPool: threadPool, logger: Logger(label: "test"))
+
+		XCTAssertNoThrow(try body(eventLoop, server))
+	}
+
+	func _testWithTemporaryFile(content: String, openFlags: OpenFlags, _ body: (String, String, EventLoop, BaseSftpServer) throws -> Void) {
+		__withServer { eventLoop, server in
+			XCTAssertNoThrow(try withTemporaryFile(content: content) { _, filePath in
+				// Open the temporary file
+				let sftpHandleString = __openFile(filePath: filePath, openFlags: openFlags, eventLoop: eventLoop, server: server)
+
+				XCTAssertNoThrow(try body(sftpHandleString, filePath, eventLoop, server))
+
+				__closeFile(sftpHandleString: sftpHandleString, eventLoop: eventLoop, server: server)
+			})
+		}
+	}
 }
